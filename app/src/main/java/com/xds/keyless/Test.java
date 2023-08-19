f14caab0ccbf9401ee246b5acbd2586e4ec19d86
xwg <416667938@qq.com>
2023-08-19 15:19:09 +0800

    创建

---
+++ app/src/main/java/com/xds/keyless/Test.java
@@ -0,0 +1,11 @@
+package com.xds.keyless;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author XWG
+ * @todo
+ * @date 2023/8/19.
+ */
+public class Test extends AppCompatActivity {
+}
