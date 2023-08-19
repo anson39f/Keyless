e48050bdf7cf9a1a1d315a35718a6fa687977a5e
xwg <416667938@qq.com>
2023-08-19 15:58:19 +0800

    创建

--- app/src/main/java/com/xds/keyless/MainActivity.kt
+++ app/src/main/java/com/xds/keyless/MainActivity.kt
@@ -2,10 +2,12 @@ package com.xds.keyless
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.postDelayed({ Toast.makeText(this, "测试", Toast.LENGTH_LONG).show() }, 500)
     }
 }
